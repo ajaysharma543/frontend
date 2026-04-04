a8953376d9393d3a0aaa7f9b46121439c5c6ad6b
Ajay Sharma <sharmaa8203@gmail.com>
2026-04-04 17:11:09 +0530

    a

--- src/chat/Leftuser.jsx
+++ src/chat/Leftuser.jsx
@@ -223,11 +223,11 @@ function Leftuser({ setsearch, setdebounce, loading, error }) {
       </span>
     )}
 
-    {item.unreadCount > 0 && !isActive && (
-      <span className="bg-green-500 text-white text-xs px-2 py-0.5 rounded-full">
-        {item.unreadCount > 4 ? '4+' : item.unreadCount}
-      </span>
-    )}
+    {latest?.isDeleted && item.unreadCount > 0 && !isActive && (
+  <span className="bg-green-500 text-white text-xs px-2 py-0.5 rounded-full">
+    {item.unreadCount > 4 ? '4+' : item.unreadCount}
+  </span>
+)}
   </div>
 </div>
               <p className={`text-sm ${messageColor}`}>{messageText}</p>
