8c0c1ed990fed27faf10f8f49b2668578d9dd356
Ajay Sharma <sharmaa8203@gmail.com>
2026-08-21 17:58:24 +0530

    added

--- src/socket/socket.io.jsx
+++ src/socket/socket.io.jsx
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://chatapp-f2i6.onrender.com', {
+const socket = io('https://chatapp-c4vc.onrender.com', {
 // const socket = io('http://localhost:3000', {
   withCredentials: true,
 });
